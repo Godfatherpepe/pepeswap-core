e50ecefe215efb65343a722be41e69966e4a68cb
danny-jin <danny@cabinetex.com>
2021-07-14 11:38:03 +0900

    remove: 1_initial_migration.js

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -2,7 +2,6 @@ const PepeswapFactory = artifacts.require("PepeswapFactory");
 
 module.exports = function (deployer, network, accounts) {
   let currentAccount = accounts[0]
-  console.log('Current Account: ', currentAccount);
   if(network == 'testnet') {
     console.warn('WARNING: Using account[1] for testnet')
     currentAccount = accounts[1]
